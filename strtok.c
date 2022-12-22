021147309dc67e38981e4ea49c6936471b1b7f68
Timilehin Olusa <olusatimmie07@gmail.com>
2022-12-21 16:58:59 -0800

    strtok function implemented

---
+++ strtok.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strtok - tokennizes a string
+*@str: string to tokenize
+*@delim: delimeter
+*Return: pointer to the tokenized string on success
+*/
+char *_strtok(char *str, const char *delim)
+{
+	char start;
+	static char *curr = NULL;
+
+	if (str != NULL)
+		curr = str;
+	if (curr == NULL || *curr == '\0')
+		return (NULL);
+	start = curr;
+	while (*curr != '\0' && strchr(delim, *curr) == NULL)
+		curr++;
+	if (*curr != '\0')
+		*curr++ = '\0';
+
+	return (start);
+}
