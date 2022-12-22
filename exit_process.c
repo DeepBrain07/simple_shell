ee10922072470572b3265e18c2c2759e9d4c9877
Timilehin Olusa <olusatimmie07@gmail.com>
2022-12-21 17:17:45 -0800

    exit function implemented

---
+++ exit_process.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*exit_process - exits the current process
+*@args: array of strings
+*Return: returns the status on success, else failure
+*/
+int exit_process(char **args)
+{
+	int status;
+
+	if (args[1])
+		status = _atoi(args[i]);
+	else
+		status = 0;
+	_exit(status);
+}
